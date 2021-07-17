335162e4644f0da857e2885a94e80186e19c3fcb
Loic Jacquel <ljacquel@gmail.com>
2021-07-17 16:41:52 +0900

    seperate express script

--- index.js
+++ index.js
@@ -1,53 +1,13 @@
 require('dotenv').config();
-const MartingaleSyncStrategy = require('./strategy/martingaleasync');
-const MartingaleStrategy = require('./strategy/martingale');
-
-const CONFIG = {
-    symbol: 'BTCUSDT',
-    leverage:10, 
-    balance:1000,
-    targetPercent:0.004,
-    positionPercent:0.01,
-    mode:'binance'
-}
-
-const executeStrategy = () => {
-    return new Promise(async (resolve, reject) => {
-        let strategy = new MartingaleSyncStrategy(CONFIG);
-        if(strategy.init) {
-            await strategy.init();
-        }
-        strategy.start();
-        resolve();
-    });
-}
-
-  
-const start = async () => {
-
-    executeStrategy();
-    console.log("end index.js");
-
-}
-
-//------------
-//------------
 
 const express = require('express')
 const app = express()
 const port = 3000
   
-const startSite = async () => {
-    
-    app.get('/', (req, res) => {
-        res.send('Hello World!')
-    })
-    
-    app.listen(port, () => {
-        console.log(`Example app listening at http://localhost:${port}`)
-    })
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
 
-}
-
-startSite();
-
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
---
+++ index2.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const MartingaleSyncStrategy = require('./strategy/martingaleasync');
+const MartingaleStrategy = require('./strategy/martingale');
+
+const CONFIG = {
+    symbol: 'BTCUSDT',
+    leverage:10, 
+    balance:1000,
+    targetPercent:0.004,
+    positionPercent:0.01,
+    mode:'binance'
+}
+
+const executeStrategy = () => {
+    return new Promise(async (resolve, reject) => {
+        let strategy = new MartingaleSyncStrategy(CONFIG);
+        if(strategy.init) {
+            await strategy.init();
+        }
+        strategy.start();
+        resolve();
+    });
+}
+
+  
+const start = async () => {
+
+    executeStrategy();
+    console.log("end index.js");
+
+}
+
+//------------
+//------------
